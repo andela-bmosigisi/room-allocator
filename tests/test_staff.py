b1f580574d008c961c01604157a8a966f665c6ad
andela-bmosigisi <brian.mosigisi@andela.com>
2016-01-22 16:22:51 +0300

    (feature) add staff test

---
+++ tests/test_staff.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestStaff(unittest.TestCase):
+
+    def setUp(self):
+        self.staff = Staff('Martin Luther')
+
+    def tearDown(self):
+        self.staff = None
+
+    def test_staff_is_instantiated(self):
+        self.assertIsInstance(self.staff, Staff, 'Staff is not created.')
+
+    def test_staff_properties(self):
+        self.assertEquals(self.staff.name, 'Martin Luther')
